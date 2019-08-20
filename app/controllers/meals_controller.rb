cae7d9e3941d629d357fe7d4cb8a8d252020e5b7
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2019-08-20 14:16:49 +0200

    page index + function meal index created

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,5 @@
+class MealsController < ApplicationController
+def index
+  @meals = Meal.all
+end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Meal.create!(
+  name: 'Chinese soup',
+  address: '10 villa Gaudelet, Paris',
+  category: 'Chinese',
+  description: 'A delicious soup',
+  unit_price: 6,
+  quantity_max: 3,
+  user_id: 1
+)
+
+Meal.create!(
+  name: 'Ceasar salad',
+  address: '10 villa Gaudelet, Paris',
+  category: 'Healthy',
+  description: 'A Healthy salad',
+  unit_price: 5,
+  quantity_max: 3,
+  user_id: 1
+)
+
+Meal.create!(
+  name: 'Vege salad',
+  address: '10 villa Gaudelet, Paris',
+  category: 'Healthy',
+  description: 'A Healthy salad',
+  unit_price: 5,
+  quantity_max: 4,
+  user_id: 1
+)
