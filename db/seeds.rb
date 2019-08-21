aeac9b60175d0a1de30816408a268c90073b7ce2
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2019-08-21 09:56:13 +0200

    changed again seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,85 +11,85 @@ Meal.destroy_all
 Order.destroy_all
 Ingredient.destroy_all
 
-User.create!(
+w = User.create!(
   username: 'Winnie',
   email: 'ourson@gmail.com',
   password: 'testtest3',
 )
 
-User.create!(
+j = User.create!(
   username: "Javier B.",
   email: "javier@sexy.com",
   password: "bailar"
 )
 
-User.create!(
+t = User.create!(
   username: 'Tinkie Winky',
   email: 'titi@gmail.com',
   password: 'testtest4',
 )
 
-User.create!(
+p = User.create!(
   username: 'Paquito',
   email: 'pepito@gmail.com',
   password: 'testtest5',
 )
 
 
-Meal.create!(
+soup = Meal.create!(
   name: 'Chinese soup',
   address: '10 villa Gaudelet, Paris',
   category: 'Chinese',
   description: 'A delicious soup',
   unit_price: 6,
   quantity_max: 3,
-  user_id: User.first
+  user: p
 )
 
-Meal.create!(
+salad = Meal.create!(
   name: 'Ceasar salad',
   address: '10 villa Gaudelet, Paris',
   category: 'Healthy',
   description: 'A Healthy salad',
   unit_price: 5,
   quantity_max: 3,
-  user_id: User.last
+  user: t
 )
 
-Meal.create!(
+vegsalad = Meal.create!(
   name: 'Vege salad',
   address: '10 villa Gaudelet, Paris',
   category: 'Healthy',
   description: 'A Healthy salad',
   unit_price: 5,
   quantity_max: 4,
-  user_id: User.first
+  user: j
 )
 
 
 Order.create!(
   order_quantity: 1,
   pick_up_date: '2019-08-20',
-  user_id: User.first,
-  meal_id: Meal.last,
+  user: p,
+  meal: vegsalad,
 )
 
 Order.create!(
   order_quantity: 3,
   pick_up_date: '2019-08-23',
-  user_id: User.first,
-  meal_id: Meal.last,
+  user: t,
+  meal: salad,
 )
 
 Order.create!(
   order_quantity: 2,
   pick_up_date: '2019-08-23',
-  user_id: User.last,
-  meal_id: Meal.first,
+  user: p,
+  meal: soup,
 )
 
-# ingredients = ["Céréales contenant du gluten", "Crustacés", "Oeufs", "Poissons", "Arachides", "Soja", "Lait", "Fruits à coques", "Céleri", "Moutarde", "Graines de sésame"]
+ingredients = ["Céréales contenant du gluten", "Crustacés", "Oeufs", "Poissons", "Arachides", "Soja", "Lait", "Fruits à coques", "Céleri", "Moutarde", "Graines de sésame"]
 
-# ingredients.each do |i|
-#   Ingredient.create(name: i)
-# end
+ingredients.each do |i|
+  Ingredient.create!(name: i)
+end
