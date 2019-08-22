b3fd2dffe328a5b9ca882247b9ad0698b922223e
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2019-08-22 19:50:57 +0200

    added search and filter on category

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,11 +1,21 @@
 class Meal < ApplicationRecord
+  include PgSearch::Model
   belongs_to :user
   has_many :orders, dependent: :destroy
   has_many :meal_ingredients, dependent: :destroy
   has_many :ingredients, through: :meal_ingredients
   has_many :eater_users, through: :orders, source: :user
 
-  CATEGORIES = ["Chinese", "French", "Sushi", "Dessert", "Grandma " "Italian", "Chinese", "Healthy", "Moroccan", "Burger", "Vegan", "Italian", "Thaï", "Hawaïan"]
+  pg_search_scope :global_search,
+    against: [ :name, :description, :category ],
+    associated_against: {
+      user: [ :username]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+
+  CATEGORIES = ["French", "Sushi", "Dessert", "Grandma " "Italian", "Chinese", "Healthy", "Moroccan", "Burger", "Vegan", "Italian", "Thaï", "Hawaïan"]
 
   validates :name, presence: true
   validates :description, presence: true
