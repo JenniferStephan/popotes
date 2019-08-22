0f121e9fea048fc0b79ab098671504bfec2c35c4
AntoineCartier <antoinecartier.contact@gmail.com>
2019-08-22 15:42:11 +0200

    Disable meals cards front OK - Bugfix height and infos position

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2019_08_21_121543) do
     t.string "address"
     t.integer "unit_price"
     t.integer "quantity_max"
-    t.date "start_availability_date"
+    t.date "start_availability_date", default: "2019-08-20"
     t.date "end_availability_date"
     t.bigint "user_id"
     t.datetime "created_at", null: false
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2019_08_21_121543) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "order_quantity"
-    t.date "pick_up_date", default: "2019-08-21"
+    t.date "pick_up_date", default: "2019-08-20"
     t.text "order_comment"
     t.bigint "user_id"
     t.bigint "meal_id"
