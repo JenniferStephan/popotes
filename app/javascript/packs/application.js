d3cf241eed3015f199f1be023834d1b23e7d57c2
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2019-08-23 10:47:37 +0200

    cleaning up

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
-import flatpickr from "flatpickr";
+// import flatpickr from "flatpickr";
 
 
 
